343fabb2a818d0b7e4edec50a064739837f1413a
Michael West <19785650+itsmichaelwest@users.noreply.github.com>
2022-11-18 23:26:31 +0000

    Move social icons to SVG files

--- next.config.js
+++ next.config.js
@@ -115,5 +115,13 @@ module.exports = {
     i18n: {
         locales: ["en"],
         defaultLocale: "en",
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        })
+      
+        return config;
     }
 }
