9f1cb2bc60eda54466dea81504776708996d2e17
Michael West <19785650+itsmichaelwest@users.noreply.github.com>
2023-04-18 22:50:26 +0100

    Fix hero images on content pages

--- pages/work/[id].tsx
+++ pages/work/[id].tsx
@@ -58,7 +58,7 @@ const Post = ({ postData, mdxSource }) => (
                 </div>
                 {postData.featuredImage &&
                 <div className="mt-32">
-                    <Image src={`/images/${postData.id}/${postData.featuredImage}`} alt={postData.featuredImageAlt} width={100} height={70} />
+                    <Image className="w-full" src={`/images/${postData.id}/${postData.featuredImage}`} alt={postData.featuredImageAlt} width={1000} height={700} />
                 </div>
                 }
             </header>
