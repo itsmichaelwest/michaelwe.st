7711c8d718844863da826aa044129165d1611a9e
Michael West <19785650+itsmichaelwest@users.noreply.github.com>
2023-11-19 00:15:42 +0000

    Update packages and SocialIcons

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -7,16 +7,18 @@ import Dribbble from '../public/vectors/dribbble.svg'
 import Behance from '../public/vectors/behance.svg'
 import Link from 'next/link'
 
+import TwitterX from '../public/vectors/twitter-x.svg'
+
 const classes = 'h-10 w-10 mr-2 text-gray-900 dark:text-gray-100 hover:bg-white dark:hover:bg-black bg-gray-100 dark:bg-gray-900 border border-transparent hover:border-gray-100 dark:hover:border-gray-900 box-content rounded-full '
 
 const svgClasses = 'relative mx-2 scale-90 -left-0.5 top-1.5 fill-current'
 
 const SocialIcons = () => (
     <div className="flex">
-        <div className={classes + "hover:text-twitter"}>
+        <div className={classes + "hover:text-white"}>
             <Link href="https://twitter.com/itsmichaelwest">
-                <span className="sr-only">Twitter</span>
-                <Twitter className={svgClasses} />
+                <span className="sr-only">X / Twitter</span>
+                <TwitterX className="relative mx-2 scale-90 top-2 fill-current" />
             </Link>
         </div>
         <div className={classes + "hover:text-mastodon"}>
